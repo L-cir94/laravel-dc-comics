28b3cdd54be1f6f5bbc1dddb669ec7158242e46b
L-cir94 <luca.cirigliano94@hotmail.it>
2023-05-29 22:59:59 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+<ul class="nav justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link" href="/home">Homepage</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/comics">Comics DC</a>
+    </li>
+
+</ul>
+
+<!-- Tab panes -->
+
+</header>
