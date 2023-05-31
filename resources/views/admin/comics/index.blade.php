80bb2f473a05044f6ed6027bb20da3369aacbaec
L-cir94 <luca.cirigliano94@hotmail.it>
2023-05-31 16:12:18 +0200

    fill table comics in admin section

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -1,5 +1,49 @@
 @extends('layouts.app')
 @section('title', 'Table Comics')
 @section('content')
-<h1>Index</h1>
+<div class="table-responsive">
+    <table class="table table-striped
+    table-hover	
+    table-borderless
+    table-primary
+    align-middle">
+        <thead class="table-light">
+            <caption>Comics</caption>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Thumb</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Type</th>
+                <th>Sale date</th>
+                <th>Series</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                @forelse ($comics as $comic)
+                <tr class="table-primary" >
+                    <td scope="row">{{$comic->id}}</td>
+                    <td>{{$comic->title}}</td>
+                    <td><img class="img-fluid" src="{{$comic->thumb}}" alt=""></td>
+                    <td>{{$comic->description}}</td>
+                    <td>{{$comic->price}}</td>
+                    <td>{{$comic->type}}</td>
+                    <td>{{$comic->sale_date}}</td>
+                    <td>{{$comic->series}}</td>
+                    <td>VIEW / EDITE / DELETE</td>
+                </tr>
+                @empty
+                <tr class="table-primary" >
+                    <td scope="row">Campo vacio</td>
+                </tr>   
+                @endforelse
+            </tbody>
+            <tfoot>
+                
+            </tfoot>
+    </table>
+</div>
+
 @endsection
