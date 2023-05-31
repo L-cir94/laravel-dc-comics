a5620f4a91674bff499183fb715ae4f271a56f8f
L-cir94 <luca.cirigliano94@hotmail.it>
2023-05-31 15:32:18 +0200

    style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,88 @@
 <footer>
-    footer
+    <footer>
+        <div class="footer_top">
+          <div class="container">
+            <div class="row pt-4 ">
+              <div class="col-2">
+                <ul>
+                  <h4>Dc Comics</h4>
+                  <li><a href="#">Characters</a></li>
+                  <li><a href="#">Comics</a></li>
+                  <li><a href="#">Movies</a></li>
+                  <li><a href="#">TV</a></li>
+                  <li><a href="#">Games</a></li>
+                  <li><a href="#">Videos</a></li>
+                  <li><a href="#">News</a></li>
+                </ul>
+                <ul>
+                  <h4>Shop</h4>
+                  <li><a href="#">Shop DC</a></li>
+                  <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+              </div>
+              <div class="col-2">
+                <ul>
+                  <h4>Dc</h4>
+                  <li><a href="#">Terms Of Use</a></li>
+                  <li><a href="#">Privacy policy (New)</a></li>
+                  <li><a href="#">Ad Choices</a></li>
+                  <li><a href="#">Advertising</a></li>
+                  <li><a href="#">Jobs</a></li>
+                  <li><a href="#">Subscriptions</a></li>
+                  <li><a href="#">Talent Workshops</a></li>
+                  <li><a href="#">CPSC Certificates</a></li>
+                  <li><a href="#">Ratings</a></li>
+                  <li><a href="#">Shop Help</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              <div class="col-2">
+                <ul>
+                  <h4>Sites</h4>
+                  <li><a href="#">DC</a></li>
+                  <li><a href="#">MAD Magazine</a></li>
+                  <li><a href="#">DC Kids</a></li>
+                  <li><a href="#">DC Universe</a></li>
+                  <li><a href="#">DC Power Visa</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="footer_bottom">
+          <div class="container d-flex justify-content-between align-items-baseline pt-4">
+            <button class="p-2">SIGN-UP NOW!</button>
+            <div class="d-flex align-items-baseline">
+              <h4 class="m-0">Follow us</h4>
+              <ul class="d-flex gap-2">
+                <li>
+                  <a href="#">
+                    <img src="./../assets/img/footer-facebook.png" alt="">
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="./../assets/img/footer-twitter.png" alt="">
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="./../assets/img/footer-youtube.png" alt="">
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="./../assets/img/footer-pinterest.png" alt="">
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="./../assets/img/footer-periscope.png" alt="">
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,43 @@
 <header>
-<ul class="nav justify-content-center">
-    <li class="nav-item">
-        <a class="nav-link" href="/home">Homepage</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="/comics">Comics DC</a>
-    </li>
-
-</ul>
-
+    <div class="container">
+        <div class="row">
+            <div class="navbar">
+                <div class="logo">
+                    <img src="{{ URL('') }}" alt="">Logo qui
+                </div>
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/home">Homepage</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/comics">Comics DC</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Movies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tv</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Games</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Videos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Fans</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Shop</a>
+                    </li>
+            
+                </ul>
+            </div>
+        </div>
+    </div>
 <!-- Tab panes -->
 
 </header>
